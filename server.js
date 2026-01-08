79cbd2a07ef6d9ed944ab7579d01e03376c19305
huizh11 <hanghuizhi@gmail.com>
2026-01-08 12:17:42 +0800

    plswork

--- server.js
+++ server.js
@@ -57,3 +57,81 @@ app.post('/addcard', async (req, res) => {
         });
     }
 });
+
+// example route: get all sanrio characters
+app.get('/allcharacters', async(req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.sanrio');
+        res.json(rows);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message:'Server error for all sanrio characters'});
+    }
+});
+
+// Example Route: Create a new card
+app.post('/allcharacters', async (req, res) => {
+    const { character_name, character_pic } = req.body;
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute(
+            'INSERT INTO sanrio (character_name, character_pic) VALUES (?, ?)',
+            [character_name, character_pic]
+        );
+        res.status(201).json({
+            message: 'Card ' + character_name + ' added successfully'
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: 'Server error - could not add card ' + character_name
+        });
+    }
+});
+
+//update
+app.put('/allcharacters/:id', async (req, res) => {
+    const { id } = req.params;
+    const { character_name, character_pic } = req.body;
+
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+        await connection.execute(
+            'UPDATE sanrio SET character_name = ?, character_pic = ? WHERE id = ?',
+            [character_name, character_pic, id]
+        );
+
+        res.json({
+            message: 'Character updated successfully'
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: 'Server error - could not update character'
+        });
+    }
+});
+
+//delete
+app.delete('/allcharacters/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+        await connection.execute(
+            'DELETE FROM sanrio WHERE id = ?',
+            [id]
+        );
+
+        res.json({
+            message: 'Character deleted successfully'
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: 'Server error - could not delete character'
+        });
+    }
+});
+
