a886365ea511060f15cc48144c069b57f8b711e4
huizh11 <hanghuizhi@gmail.com>
2026-01-19 21:57:31 +0800

    .

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
+// include the required packages
 const express = require('express');
-const mysql = require('mysql2/promise');
-const cors = require('cors');
+const mysql = require('mysql2');
 require('dotenv').config();
+const port = 3000;
 
-const app = express();
-const port = process.env.PORT || 3000;
-
+// database config info
 const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit:0,
 };
 
-app.use(cors());
+//intialize express app
+const app = express();
+//helps app to read JSON
 app.use(express.json());
 
-app.get('/allcards', async (req, res) => {
-    try {
-        const connection = await mysql.createConnection(dbConfig);
-        const [rows] = await connection.execute('SELECT * FROM cards');
-        res.json(rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error for allcards' });
-    }
-});
-
+// start the server
 app.listen(port, () => {
     console.log('Server running on port', port);
+});
+
+// example route: get all cards
+app.get('/allcards', async(req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        res.json(rows);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message:'Server error for allcards'});
+    }
 });
