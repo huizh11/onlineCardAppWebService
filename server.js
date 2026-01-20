d6b46f24827e39c2744902c8afe0bbe064b861e8
huizh11 <hanghuizhi@gmail.com>
2026-01-20 21:39:42 +0800

    12345

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 // Secure CORS
 const allowedOrigins = [
     "http://localhost:3000",
-    "https://c219-l19-pdsjoozcr-renees-projects-73d8499a.vercel.app"
+    "https://c219-l19-q7ttp0vpm-renees-projects-73d8499a.vercel.app"
 ];
 
 
